5e732efa88e0c16694d8179d6a57fa8b336659fc
Jessica C <jycheng1@andrew.cmu.edu>
2016-06-01 11:06:24 -0400

    Items in the database are displayed on screen

--- insert.php
+++ insert.php
@@ -1,16 +1,18 @@
 <html>
 <body>
 <?php
-  require_once('../../secureHtdocs/conn.php');
-  $sql = "INSERT INTO resources(requestDate, requestSummary, organization, address, lattitude, longitude, priority)
-  VALUES ('$date', '$_POST[request]','$_POST[organization]', '$_POST[address]',0,0,1)";
-
+require_once('../../secureHtdocs/conn.php'); // connect to socius database
+  
+// insert into sql database
+$sql = "INSERT INTO resources(requestDate, requestSummary, organization, 
+	  address, lattitude, longitude, priority)  VALUES ('$date', 
+	  '$_POST[request]','$_POST[organization]', '$_POST[address]',0,0,1)";
   if ($conn->query($sql) === TRUE) {
       echo "New record created successfully";
   } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
   }
-
+//mysqli_close($conn);
 ?>
 </body>
 </html>
