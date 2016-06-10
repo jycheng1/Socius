b74623008899ccf281aefe1909540f71125818e7
Jessica C <jycheng1@andrew.cmu.edu>
2016-06-10 17:58:10 -0400

    submit button now shows missing elements and correctly adds to database

--- insert.php
+++ insert.php
@@ -1,56 +1,71 @@
 <?php
-//if(isset($_POST['submit'])){
-  //$data_missing = array();
-  //if(empty($_POST['request'])){
+include('../../secureHtdocs/conn.php'); // connect to socius database
+$data_missing = array();
+$isFilled = true;
+// deals with request summary box
+  if(empty($_POST['request'])){
     // Add request date to array
-  //  $data_missing[] = 'request summary';  
- // } else{
+    $data_missing[] = 'request summary';  
+  } else{
     // trim white space
     $r_sum = trim($_POST['request']);
-  //} 
-  //if(empty($_POST['organization'])){
+  } 
+
+// deals with org name box
+  if(empty($_POST['organization'])){
     // Add request date to array
-  //  $data_missing[] = 'organization';  
-  //} else{
+    $data_missing[] = 'organization';  
+  } else{
     // trim white space
     $org = trim($_POST['organization']);
-  //}
- // if(empty($_POST['address'])){
+  }
+
+// deals with address box
+  if(empty($_POST['address'])){
     // Add request date to array
- //   $data_missing[] = 'address';  
- // } else{
+    $data_missing[] = 'address';  
+  } else{
     // trim white space
     $add = trim($_POST['address']);
- // }
-//  if(empty($data_missing)){
-    require_once('../../secureHtdocs/conn.php'); // connect to socius database
-  
+  }
+  $a = array();
+  // enters this if no data is missing
+  if(empty($data_missing)){
+ 
     // insert into sql database
+    
     $query = "INSERT INTO resources(requestDate, requestSummary, organization, 
             address, lattitude, longitude, priority)  
             VALUES (NOW(), ?, ?, ?,0,0,1)";
-    
+
     $stmt = mysqli_prepare($conn, $query);
     mysqli_stmt_bind_param($stmt, "sss", $r_sum, $org, $add);
     mysqli_stmt_execute($stmt);
     $affected_rows = mysqli_stmt_affected_rows($stmt);
+
     if(!$affected_rows == 1){
       echo 'Error Occured<br />';
       echo mysqli_error();
       mysqli_stmt_close($stmt);
       mysqli_close($conn);
     }
-  //} 
-    /*
-  else{
-      echo '<strong>You need to enter the following data:</strong> <br />';
-      foreach($data_missing as $missing){
-        echo "<strong>-$missing</strong><br />";
-      }
-      echo"<br>";
+    mysqli_close($conn);
+
+    $isFilled=true;
+    $a = array( 'isFilled' => true,
+        'message' => "none");
 
   }
-  */
-//}
-echo "HI";
+  // missing data is not empty, so you have missing form elements
+  else{
+    $isFilled=false;
+    $message = "You need to enter the following data: \n";
+    foreach($data_missing as $missing){
+      $message = $message . $missing . "\n";
+    }
+    $a = array( 'isFilled' => false,
+      'message' => $message);
+    mysqli_close($conn);
+  }
+  echo json_encode($a);
 ?>
--- submit.js
+++ submit.js
@@ -23,8 +23,18 @@ function f1(){
       type: 'POST',
       data: dataS,
       url: 'insert.php',
+      dataType: 'json',
       success: function(data){
+        if (!data['isFilled']){
+          document.getElementById('errorMsg').innerHTML=data["message"];
+        }
+        if(data['isFilled']){
+          document.getElementById('errorMsg').innerHTML="";
+        }
         callBack(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        alert(jqXHR.status);
       }
       });
 }
