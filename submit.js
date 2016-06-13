60290d0db51047200e5b1f00d465a323be2abbed
Jessica C <jycheng1@andrew.cmu.edu>
2016-06-13 14:21:56 -0400

    form clears after successfully submitting

--- submit.js
+++ submit.js
@@ -1,4 +1,5 @@
-function f1(){
+// is called when the form is submitted
+function submitForm(){
   var dataS = $("#request_form").serialize();
   $.ajax({
       type: 'POST',
@@ -7,12 +8,17 @@ function f1(){
       dataType: 'json',
       success: function(data){
         if (!data['isFilled']){
+          // there are parts of the form that aren't filled
           document.getElementById('errorMsg').innerHTML=data["message"];
         }
         if(data['isFilled']){
+          // all text boxes are filled
           document.getElementById('errorMsg').innerHTML="";
+          // erases the text in forms after you submit
+          $('input[type="text"], textarea').val('');
+          $('input[type="text"], text').val('');
         }
-        sortI(document.getElementById("sortOp").value);
+        sortI(document.getElementById("sortOp").value); // updates the table
       },
       error: function(jqXHR, textStatus, errorThrown){
         alert(jqXHR.status);
