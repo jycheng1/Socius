18247ebc68d0af55fa1602d562734b2e1870af7e
Jessica C <jycheng1@andrew.cmu.edu>
2016-06-29 15:08:09 -0400

    requesting toggles

--- colorChange.js
+++ colorChange.js
@@ -1,6 +1,27 @@
-function grays(){
-  document.getElementById('or1').setAttribute("class", "style1");
-  $('#request').click(function(){
-    $('#or1').addClass('greyOut');
+$(document).ready(function(){
+  $("#request").click(function () {
+    if ($(this).text() == "Request"){
+      $(this).text("Added");
+      $("#img1").css({
+        'border': "solid 2px red",
+        /*'webkit-filter':'grayscale(100%)'*/
+      });
+      $("#b1").css({
+        'background' : '#ececec'
+      });
+    }
+    else{
+      $(this).text("Request");
+      $("#img1").css({
+/*
+        'webkit-filter': 'blur(3px)',
+*/
+        'border': "solid 2px white",
+/*        'webkit-filter':'grayscale(0%)' */
+      });
+      $("#b1").css({
+        'background' : 'white'
+      });
+    };
   });
-}
+});
