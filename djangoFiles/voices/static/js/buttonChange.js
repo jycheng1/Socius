81024d93c36379824cd968b16d1e478b94b9b145
Jessica C <jycheng1@andrew.cmu.edu>
2016-07-07 16:13:18 -0400

    selection works for all the cards

--- djangoFiles/voices/static/js/buttonChange.js
+++ djangoFiles/voices/static/js/buttonChange.js
@@ -1,32 +1,37 @@
 $(document).ready(function(){
-  $("#add").click(function(){
+  $(".add").click(function(){
     if ($(this).text() == "Add"){
       $(this).text("Added");
+      /*
       $("#img1").css({
-        /*
+        
         'webkit-filter':'grayscale(100%)'
         'webkit-filter':'blur(5px)'
-        */
-
-        
       });
-      $("#1").css({
+       */
+     var buttonId = "#".concat(this.name)
+      $(buttonId).css({
         'border': "solid 2px green",
         '-webkit-box-shadow':'0px 0px 7px #000000',
         '-moz-box-shadow':'0px 0px 7px #000000',
       });
+
     }
     else{
       $(this).text("Add");
-      $(".img1").css({
+      /*
+      $("#img1").css({
         'webkit-filter':'blur(0px)'
       });
-      $("#1").css({
+       */
+      var buttonId = "#".concat(this.name)
+      $(buttonId).css({
         'border': "solid 2px white",
         '-webkit-box-shadow': 'none',
         '-moz-box-shadow':'none',
         'box-shadow':'0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12)',
       });
+     
     };
   });
 });
